a3cfb2b1c22ec03a9e2f96d212aae31de4646d0c
Nestor Arocha <nesaro@gmail.com>
2013-08-25 08:50:40 +0100

    added setup.py, rearranged files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='regexps',
+      version='0.1.0',
+      description='regular expresion list',
+      author='Nestor Arocha',
+      author_email='nesaro@gmail.com',
+      url='https://github.com/nesaro/regexps',
+      packages = find_packages(),
+     )
+
